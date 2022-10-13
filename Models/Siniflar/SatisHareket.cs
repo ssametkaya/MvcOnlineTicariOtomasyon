7056300e756ef7c08014d81c3615008bfc6c747b
Süleyman Samet Kaya <ssametkaya52@gmail.com>
2022-10-13 23:41:31 +0300

    Sınıflar oluşturuldu

---
+++ Models/Siniflar/SatisHareket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class SatisHareket
+    {
+        [Key]
+        public int SatisId { get; set; }
+        public DateTime Tarih { get; set; }
+        public int Adet { get; set; }
+        public decimal Fiyat { get; set; }
+        public decimal Toplam { get; set; }
+
+        public ICollection<Urun> Uruns { get; set; }
+        public ICollection<Personel> Personels{ get; set; }
+        public ICollection<Cariler> Carilers{ get; set; }
+    }
+}
